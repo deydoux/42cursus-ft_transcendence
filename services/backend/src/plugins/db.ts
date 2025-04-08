f4ced97c9d56d3c9c68381a5e5731ccf846666a1
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-04-08 13:49:12 +0200

    feat(database): update DB_PATH handling to set default in development environment

--- services/backend/src/plugins/db.ts
+++ services/backend/src/plugins/db.ts
@@ -3,16 +3,26 @@ import {open} from 'sqlite';
 import * as sqlite3 from 'sqlite3';
 import fp from 'fastify-plugin';
 
-const DB_FILE = process.env.DB_FILE || 'ft_transcendence.db';
+const {NODE_ENV} = process.env;
+let {DB_PATH} = process.env;
 
 const plugin: FastifyPluginAsync = async server => {
+  if (!DB_PATH) {
+    const message = 'DB_PATH environment variable is not set';
+
+    if (NODE_ENV === 'development') {
+      DB_PATH = 'ft_transcendence.db';
+      server.log.warn(`${message}, using as default: ${DB_PATH}`);
+    } else throw new Error(message);
+  }
+
   const db = await open({
-    filename: DB_FILE,
+    filename: DB_PATH,
     driver: sqlite3.verbose().Database,
   });
 
   db.on('trace', (sql: string) => {
-    server.log.trace(`SQL: ${sql}`);
+    server.log.trace(`${DB_PATH}: ${sql}`);
   });
 
   server.decorate('db', db);
