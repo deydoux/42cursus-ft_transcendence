312b391c9c95991139dc0db88311815c996976c1
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-04-22 15:47:42 +0200

    refactor: update project configuration and dependencies
    - Removed `files` field from package.json.
    - Updated linting scripts to use ESLint instead of gts.
    - Added client and server specific linting and fixing scripts.
    - Updated devDependencies: added ESLint, Prettier, and related packages; removed gts.
    - Deleted tsconfig.client.json as it is no longer needed.
    - Updated tsconfig.json to extend TypeScript options and added stricter compiler options.
    - Added new Vite configuration file for client-side development with Tailwind CSS support.

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,16 @@ export default defineConfig(() => {
   const env = loadEnv('', process.cwd());
 
   const DEV = env.NODE_ENV === 'development';
-  const PROD = !DEV;
 
   return {
     root: 'src/client',
+    plugins: [tailwindcss()],
     mode: DEV ? 'development' : 'production',
     build: {
       outDir: '../../dist',
       emptyOutDir: true,
-      minify: PROD,
+      minify: false,
+      sourcemap: DEV,
     },
-    plugins: [tailwindcss()],
   };
 });
