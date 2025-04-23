e3c90071f2c79851a7640f9e9369cc4cdc3292b5
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-04-23 14:35:55 +0200

    feat: enhance connections retrieval by improving data mapping and sorting

--- src/server/routes/api/account/connections/GET.ts
+++ src/server/routes/api/account/connections/GET.ts
@@ -8,9 +8,17 @@ const plugin: FastifyPluginAsync = async server => {
     {onRequest: server.authenticate},
     async (request, reply) => {
       const {id} = request.user;
-      const connections = await server.db.all(
-        SQL`SELECT id, ip, user_agent AS userAgent, created_at AS createdAt, updated_at AS updatedAt FROM connections WHERE user_id = ${id}`,
-      );
+      const connections = (
+        await server.db.all(
+          SQL`SELECT id, ip, user_agent AS userAgent, created_at AS createdAt, updated_at AS updatedAt FROM connections WHERE user_id = ${id}`,
+        )
+      )
+        .map(connection => ({
+          ...connection,
+          createdAt: new Date(connection.createdAt * 1000),
+          updatedAt: new Date(connection.updatedAt * 1000),
+        }))
+        .sort((a, b) => b.updatedAt.getTime() - a.updatedAt.getTime());
 
       return reply.send({currentConnection: request.connection, connections});
     },
