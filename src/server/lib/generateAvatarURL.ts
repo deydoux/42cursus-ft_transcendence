c8a2bc9e66495de625c6dec9f5f8778e0eb6253f
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-05-05 16:22:30 +0200

    refactor: enforce required fields in generateAvatarURL and simplify avatar assignment

--- src/server/lib/generateAvatarURL.ts
+++ src/server/lib/generateAvatarURL.ts
@@ -1,15 +1,14 @@
 export default function generateAvatarURL(user: {
   id: number;
-  has_avatar?: boolean;
-  avatar_version?: number;
-  avatar?: string;
+  has_avatar: boolean;
+  avatar_version: number;
 }) {
   const {id, has_avatar: hasAvatar, avatar_version: version} = user;
-  delete user.has_avatar;
-  delete user.avatar_version;
 
-  if (hasAvatar) user.avatar = `/api/users/${id}/avatar?v=${version}`;
-  else user.avatar = '/static/default_avatar.webp';
-
-  return user;
+  return {
+    ...user,
+    avatar: hasAvatar
+      ? `/api/users/${id}/avatar?v=${version}`
+      : '/static/default_avatar.webp',
+  };
 }
