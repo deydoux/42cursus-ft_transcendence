6a8dac106e3de81c4486572a4110d4f5fa02e3e9
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-05-06 13:58:42 +0200

    feat: implement TOTP removal endpoint and update TOTP enabling logic

--- src/server/routes/api/account/totp/delete.ts
+++ src/server/routes/api/account/totp/delete.ts
@@ -1,6 +1,6 @@
 import {FastifyPluginAsyncJsonSchemaToTs} from '@fastify/type-provider-json-schema-to-ts';
-import SQL from 'sql-template-strings';
 import {TOTP} from 'otpauth';
+import SQL from 'sql-template-strings';
 
 const schema = {
   body: {
@@ -13,25 +13,23 @@ const schema = {
 };
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async server => {
-  server.put('/', {schema}, async (request, reply) => {
+  server.delete('/', {schema}, async (request, reply) => {
     const {id} = request.user;
     const account = await server.db.get(
       SQL`SELECT totp_enabled AS totp, totp_secret AS secret FROM users WHERE id = ${id}`,
     );
 
     if (!account) return reply.notFound('Account not found');
-    if (account.totp) return reply.badRequest('TOTP already enabled');
+    if (!account.totp) return reply.badRequest('TOTP already disabled');
 
     const {secret} = account;
-    if (!secret) return reply.badRequest('TOTP secret not generated');
-
     const totp = new TOTP({secret});
     const {token} = request.body;
     if (totp.validate({token}) === null)
       return reply.badRequest('Invalid TOTP code');
 
     await server.db.run(
-      SQL`UPDATE users SET totp_enabled = TRUE, totp_secret = ${secret} WHERE id = ${id}`,
+      SQL`UPDATE users SET totp_enabled = 0, totp_secret = NULL WHERE id = ${id}`,
     );
 
     return reply.code(204).send();
