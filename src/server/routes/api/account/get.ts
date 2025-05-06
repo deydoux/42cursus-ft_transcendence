4df2b59cdcc69bed7b0c1dbb56102b90fb69e529
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-05-06 13:12:13 +0200

    feat: refactor account retrieval to include connections and TOTP status

--- src/server/routes/api/account/get.ts
+++ src/server/routes/api/account/get.ts
@@ -5,16 +5,29 @@ import generateAvatarURL from '#lib/generateAvatarURL';
 const plugin: FastifyPluginAsync = async server => {
   server.get('/', async (request, reply) => {
     const {id} = request.user;
-    const account = await server.db.get(
-      SQL`SELECT id, username, has_avatar, avatar_version, password_edited_at AS passwordEditedAt FROM users WHERE id = ${id}`,
-    );
+    const {connection} = request;
+
+    const [account, connections] = await Promise.all([
+      server.db.get(
+        SQL`SELECT id, username, totp_enabled AS totp, has_avatar, avatar_version, password_edited_at AS passwordEditedAt FROM users WHERE id = ${id}`,
+      ),
+      server.db.all(
+        SQL`SELECT id, ip, user_agent AS userAgent, created_at AS createdAt, updated_at AS updatedAt FROM connections WHERE user_id = ${id} ORDER BY updated_at DESC`,
+      ),
+    ]);
 
     if (!account) return reply.notFound('Account not found');
 
+    account.totp = Boolean(account.totp);
     account.passwordEditedAt = new Date(account.passwordEditedAt * 1000);
     generateAvatarURL(account);
 
-    return reply.send(account);
+    connections.forEach(connection => {
+      connection.createdAt = new Date(connection.createdAt * 1000);
+      connection.updatedAt = new Date(connection.updatedAt * 1000);
+    });
+
+    return reply.send({...account, connection, connections});
   });
 };
 
