ab882c887874a15b3cf08a7e66ae2e240dc55e70
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-05-13 13:53:50 +0200

    feat: add various decorators for authentication, avatar management, and user validation

--- src/server/index.ts
+++ src/server/index.ts
@@ -34,11 +34,6 @@ async function main() {
   await server.register(import('@fastify/sensible'));
 
   await server.register(autoload, {
-    dir: join(__dirname, 'decorators'),
-    encapsulate: false,
-  });
-
-  await server.register(autoload, {
     dir: join(__dirname, 'plugins'),
     encapsulate: false,
   });
