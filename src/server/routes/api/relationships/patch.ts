87df7a2c92e68e01129ea3847eba1dd65526e6f9
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-05-13 13:33:18 +0200

    feat: implement patch endpoint for updating relationship type to 'friend'

--- src/server/routes/api/relationships/patch.ts
+++ src/server/routes/api/relationships/patch.ts
@@ -1,8 +1,40 @@
 import {FastifyPluginAsyncJsonSchemaToTs} from '@fastify/type-provider-json-schema-to-ts';
+import SQL from 'sql-template-strings';
 import {idParamsSchema as schema} from '#lib/schemas';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async server => {
-  server.patch('/:id', {schema}, async (request, reply) => {});
+  server.patch('/:id', {schema}, async (request, reply) => {
+    const {id: userID} = request.user;
+    const {id} = request.params;
+
+    const relationship = await server.db.get(
+      SQL`SELECT other_id AS otherID, type
+          FROM relationships
+          WHERE id = ${id} AND (user_id = ${userID} OR other_id = ${userID})`,
+    );
+
+    if (!relationship) return reply.notFound('Relationship not found');
+
+    if (relationship.otherID !== userID)
+      return reply.badRequest('Cannot change your own relationship');
+
+    if (relationship.type !== 'pending') {
+      if (relationship.type === 'block')
+        return reply.notFound('Relationship not found');
+
+      return reply.badRequest(
+        `Cannot change ${relationship.type} relationship`,
+      );
+    }
+
+    await server.db.run(
+      SQL`UPDATE relationships
+          SET type = 'friend'
+          WHERE id = ${id} AND type = 'pending' AND other_id = ${userID}`,
+    );
+
+    return reply.code(204).send();
+  });
 };
 
 export default plugin;
