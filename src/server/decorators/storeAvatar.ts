4fefd8c83e905a37949909eb961338168b5e9774
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-05-13 11:00:24 +0200

    refactor: improve logging for avatar resizing and file operations

--- src/server/decorators/storeAvatar.ts
+++ src/server/decorators/storeAvatar.ts
@@ -21,17 +21,24 @@ const plugin: FastifyPluginAsync = async server => {
       throw server.httpErrors.badRequest(error.message);
     });
     const size = Math.min(height ?? 0, width ?? 0, 1024);
+
+    server.log.trace(
+      `Resizing avatar from ${width}x${height} to ${size}x${size}`,
+    );
     avatar.resize(size, size);
 
     const cacheFile = join(server.paths.cache, `avatar_${++it}.webp`);
     const avatarFile = join(server.paths.avatars, `${id}.webp`);
+
+    server.log.trace(`Writing avatar to ${cacheFile}`);
     await avatar.toFile(cacheFile);
+
+    server.log.trace(`Renaming avatar to ${avatarFile}`);
     await rename(cacheFile, avatarFile);
 
     await server.db.run(
       SQL`UPDATE users SET has_avatar = TRUE, avatar_version = avatar_version + 1 WHERE id = ${id}`,
     );
-    console.log(cacheFile);
   });
 };
 
