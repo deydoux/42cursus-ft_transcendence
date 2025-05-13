09db8abf7d67e2ade01b0d5ff51e9c2f62e55d4e
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-05-13 11:19:51 +0200

    refactor: simplify user account retrieval by removing connections query

---
+++ src/server/routes/api/account/connections/get.ts
@@ -0,0 +1,26 @@
+import {FastifyPluginAsync} from 'fastify';
+import SQL from 'sql-template-strings';
+
+const plugin: FastifyPluginAsync = async server => {
+  server.get('/', async (request, reply) => {
+    const {id} = request.user;
+    const {connection} = request;
+
+    const connections = await server.db.all(
+      SQL`SELECT id, ip, user_agent AS userAgent, created_at AS createdAt,
+                 updated_at AS updatedAt
+          FROM connections
+          WHERE user_id = ${id}
+          ORDER BY updated_at DESC`,
+    );
+
+    connections.forEach(connection => {
+      connection.createdAt = new Date(connection.createdAt * 1000);
+      connection.updatedAt = new Date(connection.updatedAt * 1000);
+    });
+
+    return reply.send({connection, connections});
+  });
+};
+
+export default plugin;
--- src/server/routes/api/account/get.ts
+++ src/server/routes/api/account/get.ts
@@ -5,16 +5,13 @@ import generateAvatarURL from '#lib/generateAvatarURL';
 const plugin: FastifyPluginAsync = async server => {
   server.get('/', async (request, reply) => {
     const {id} = request.user;
-    const {connection} = request;
 
-    const [user, connections] = await Promise.all([
-      server.db.get(
-        SQL`SELECT id, username, password_edited_at AS passwordEditedAt, totp_enabled AS totp, has_avatar, avatar_version FROM users WHERE id = ${id}`,
-      ),
-      server.db.all(
-        SQL`SELECT id, ip, user_agent AS userAgent, created_at AS createdAt, updated_at AS updatedAt FROM connections WHERE user_id = ${id} ORDER BY updated_at DESC`,
-      ),
-    ]);
+    const user = await server.db.get(
+      SQL`SELECT id, username, password_edited_at AS passwordEditedAt,
+                 totp_enabled AS totp, has_avatar, avatar_version
+          FROM users
+          WHERE id = ${id}`,
+    );
 
     if (!user) return reply.notFound('Account not found');
 
@@ -23,12 +20,7 @@ const plugin: FastifyPluginAsync = async server => {
       user.passwordEditedAt = new Date(user.passwordEditedAt * 1000);
     generateAvatarURL(user);
 
-    connections.forEach(connection => {
-      connection.createdAt = new Date(connection.createdAt * 1000);
-      connection.updatedAt = new Date(connection.updatedAt * 1000);
-    });
-
-    return reply.send({...user, connection, connections});
+    return reply.send(user);
   });
 };
 
