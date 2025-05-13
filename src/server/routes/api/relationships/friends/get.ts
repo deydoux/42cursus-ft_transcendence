cdb56395fa8fa5e4b380e30531562e7284d2bd0d
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-05-13 10:45:59 +0200

    feat: update friend request API to use JSON body and enhance friend retrieval query

--- src/server/routes/api/relationships/friends/get.ts
+++ src/server/routes/api/relationships/friends/get.ts
@@ -1,21 +1,25 @@
 import {FastifyPluginAsync} from 'fastify';
 import SQL from 'sql-template-strings';
+import generateAvatarURL from '#lib/generateAvatarURL';
 
 const plugin: FastifyPluginAsync = async server => {
   server.get('/', async (request, reply) => {
     const {id} = request.user;
 
     const friends = await server.db.all(
-      SQL`SELECT r.id, u.id, u.username, u.avatar
+      SQL`SELECT r.id AS relationshipID, u.id, u.username, u.last_seen AS lastSeen, u.has_avatar, u.avatar_version
           FROM relationships r
-          JOIN users u ON u.id = r.other_id
-          WHERE u.id = ${id} AND r.type = 'friend'
-          UNION
-          SELECT r.id, u.id, u.username, u.avatar
-          FROM relationships r
-          JOIN users u ON u.id = r.user_id
-          WHERE u.id = ${id} AND r.type = 'friend'`,
+          JOIN users u ON (r.user_id = ${id} AND u.id = r.other_id) OR
+                          (r.other_id = ${id} AND u.id = r.user_id)
+          WHERE r.type = 'friend' AND (r.user_id = ${id} OR r.other_id = ${id})`,
     );
+
+    friends.forEach(friend => {
+      generateAvatarURL(friend);
+      friend.lastSeen = new Date(friend.lastSeen * 1000);
+      friend.online = server.clients.isUserOnline(friend.id);
+      // return {...friend, online: server.clients.isUserOnline(friend.id)};
+    });
 
     return reply.send({friends});
   });
