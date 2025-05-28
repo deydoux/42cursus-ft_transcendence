37db70a561e0aa333192dc22ecd77481bbe72d4d
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-05-28 18:06:15 +0200

    refactor: improve chat retrieval query and enhance user avatar serialization

--- src/server/routes/api/chats/get.ts
+++ src/server/routes/api/chats/get.ts
@@ -1,14 +1,15 @@
 import {FastifyPluginAsync} from 'fastify';
 import SQL from 'sql-template-strings';
+import serializeUserAvatar from '#lib/serializeUserAvatar';
 
 const plugin: FastifyPluginAsync = async server => {
   server.get('/', async (request, reply) => {
     const {id} = request.user;
 
     const chats = await server.db.all(SQL`
-      SELECT r.updated_at AS updatedAt,
+      SELECT COALESCE(dm.created_at, r.updated_at) AS updatedAt,
              u.id, username, last_seen AS lastSeen, has_avatar, avatar_version,
-             message, dm.created_at AS updatedAt,
+             message,
              (
                SELECT COUNT(*)
                FROM direct_messages
@@ -18,7 +19,7 @@ const plugin: FastifyPluginAsync = async server => {
       JOIN users u
       ON (user_id = ${id} AND u.id = other_id)
          OR (other_id = ${id} AND u.id = user_id)
-      JOIN direct_messages dm
+      LEFT JOIN direct_messages dm
       ON dm.id = (
                     SELECT id
                     FROM direct_messages
@@ -27,7 +28,15 @@ const plugin: FastifyPluginAsync = async server => {
                     ORDER BY created_at DESC
                     LIMIT 1
                  )
-      WHERE type = 'friend' AND (user_id = ${id} OR other_id = ${id})`);
+      WHERE type = 'friend' AND (user_id = ${id} OR other_id = ${id})
+      ORDER BY updatedAt DESC`);
+
+    chats.forEach(chat => {
+      serializeUserAvatar(chat);
+      chat.updatedAt = new Date(chat.updatedAt * 1000);
+      chat.lastSeen = new Date(chat.lastSeen * 1000);
+      chat.online = server.clients.isUserOnline(chat.id);
+    });
 
     return reply.send(chats);
   });
