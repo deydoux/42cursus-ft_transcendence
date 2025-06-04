9586720665a8d2234eb07980ff07149ea708bfd0
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-06-04 09:35:48 +0000

    refactor: improve message sending logic and enhance user checks

--- src/server/routes/api/chats/post.ts
+++ src/server/routes/api/chats/post.ts
@@ -1,4 +1,6 @@
+import serializeUserAvatar from '#lib/serializeUserAvatar';
 import {FastifyPluginAsyncJsonSchemaToTs} from '@fastify/type-provider-json-schema-to-ts';
+import {serialize} from 'node:v8';
 import SQL from 'sql-template-strings';
 
 const schema = {
@@ -14,19 +16,43 @@ const schema = {
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async server => {
   server.post('/', {schema}, async (request, reply) => {
+    const sender = await server.db.get(SQL`
+      SELECT id, username, has_avatar, avatar_version
+      FROM users
+      WHERE id = ${request.user.id}
+    `);
+    serializeUserAvatar(sender);
+
     const recipient = await server.db.get(SQL`
       SELECT id
       FROM users
       WHERE id = ${request.body.recipientID}
     `);
 
-    if (!recipient) return reply.notFound('Recipient not found');
-    if (recipient.id === request.user.id)
+    if (!recipient) return reply.notFound('User not found');
+    if (sender.id === recipient.id)
       return reply.badRequest('Cannot send message to yourself');
 
-    // const relationship = await server.db.get(SQL`
-    //   SELECT type
-    //   FROM relationships
+    const relationship = await server.db.get(SQL`
+      SELECT type
+      FROM relationships
+      WHERE user_id = ${sender.id} AND other_id = ${recipient.id}
+    `);
+
+    if (relationship?.type === 'block')
+      return reply.conflict('You have blocked this user');
+
+    const otherRelationship = await server.db.get(SQL`
+      SELECT type
+      FROM relationships
+      WHERE user_id = ${recipient.id} AND other_id = ${sender.id}
+    `);
+
+    if (otherRelationship?.type === 'block')
+      return reply.notFound('User not found');
+
+    if (relationship?.type !== 'friend' && otherRelationship?.type !== 'friend')
+      return reply.badRequest('You can only send messages to friends');
   });
 };
 
