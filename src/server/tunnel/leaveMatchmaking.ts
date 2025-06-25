28444e819e9ff9d2faa0823039b7b46f6feac548
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-06-25 15:40:22 +0000

    feat: implement leave matchmaking functionality with mode validation

--- src/server/tunnel/leaveMatchmaking.ts
+++ src/server/tunnel/leaveMatchmaking.ts
@@ -5,4 +5,22 @@ export default function leaveMatchmaking(
   server: FastifyInstance,
   client: Client,
   message: object,
-) {}
+) {
+  const {mode} = message as {mode: string};
+
+  if (!mode)
+    return client.socket.send(
+      server.clients.message({type: 'error', message: 'Mode is required'}),
+    );
+
+  switch (mode) {
+    case 'casual':
+      if (server.queues.casual?.socket === client.socket)
+        server.queues.casual = null;
+      break;
+    default:
+      return client.socket.send(
+        server.clients.message({type: 'error', message: 'Invalid mode'}),
+      );
+  }
+}
