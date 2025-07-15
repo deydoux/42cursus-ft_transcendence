5325dc9544d3db26d8fdec31a37428f8aa52e53c
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-07-15 12:43:05 +0200

    fix: remove unused destroy method and related TODO in PongMatch class

--- src/server/lib/PongMatch.ts
+++ src/server/lib/PongMatch.ts
@@ -1,11 +1,6 @@
 import Match, {Player} from '#lib/Match';
 
 export default class PongMatch extends Match {
-  protected destroy() {
-    this.server.log.warn('TODO: Save match results in database');
-    super.destroy();
-  }
-
   protected handleMove(player: Player, message: object) {
     this.server.log.warn('TODO: Handle player move in Pong');
   }
