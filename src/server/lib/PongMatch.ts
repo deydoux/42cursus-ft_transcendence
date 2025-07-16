be8c07e450799e9359ccc26d929b35b928c83852
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-07-16 13:46:23 +0200

    feat: add sleep function to delay game tick in PongMatch

--- src/server/lib/PongMatch.ts
+++ src/server/lib/PongMatch.ts
@@ -2,6 +2,11 @@ import Match, {Player} from '#lib/Match';
 import {Client} from '#types/Clients';
 import {FastifyInstance} from 'fastify';
 
+// TODO: Remove
+const sleep = (seconds: number): Promise<void> => {
+  return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+};
+
 export default class PongMatch extends Match {
   constructor(server: FastifyInstance, players: [Client, Client]) {
     super(server, players, 'pong');
@@ -12,11 +17,12 @@ export default class PongMatch extends Match {
   }
 
   protected async tick() {
+    await sleep(1);
     this.server.log.warn('TODO: Implement game logic for Pong');
 
-    this.winner =
-      this.players[0].userID < this.players[1].userID
-        ? this.players[0]
-        : this.players[1];
+    // this.winner =
+    //   this.players[0].userID < this.players[1].userID
+    //     ? this.players[0]
+    //     : this.players[1];
   }
 }
