1c91493676f6756d5772c17cd2f31bd216e0a96f
Dorian Eydoux <bonjour@dorian.cool>
2025-07-30 23:33:33 +0200

    refactor: update tunnel command and improve authorization handling

--- src/server/routes/api/tunnel.ts
+++ src/server/routes/api/tunnel.ts
@@ -3,7 +3,11 @@ import {FastifyPluginAsync} from 'fastify';
 const plugin: FastifyPluginAsync = async server => {
   await server.register(import('@fastify/websocket'));
 
-  server.addHook('onRequest', server.authenticate());
+  server.addHook('onRequest', async request => {
+    if (!request.headers.authorization)
+      request.headers.authorization = request.headers['sec-websocket-protocol'];
+    await server.authenticate()(request);
+  });
 
   server.get('/tunnel', {websocket: true}, server.clients.routeHandler);
 };
