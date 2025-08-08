d279fe7d8e078208986ec5af95bcd80837b55453
Dorian Eydoux <bonjour@dorian.cool>
2025-08-08 12:04:56 +0200

    Remove confirmPassword field from account edit API

--- src/server/routes/api/account/patch.ts
+++ src/server/routes/api/account/patch.ts
@@ -11,7 +11,6 @@ const schema = {
     properties: {
       username,
       password,
-      confirmPassword: {type: 'string'},
       oldPassword: {type: 'string'},
     },
   } as const,
@@ -19,12 +18,7 @@ const schema = {
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async server => {
   server.patch('/', {schema}, async (request, reply) => {
-    const {username, password, confirmPassword, oldPassword} = request.body;
-    if (password !== confirmPassword)
-      throw {
-        ...errorCodes.FST_ERR_VALIDATION('Password mismatch'),
-        field: 'confirmPassword',
-      };
+    const {username, password, oldPassword} = request.body;
 
     const {id} = request.user;
     const columns = [];
