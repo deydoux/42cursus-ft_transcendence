e46ddb2ea0dace5fde6c8d69c51872f9035e7913
Dorian Eydoux <bonjour@dorian.cool>
2025-08-12 12:27:27 +0200

    Fix import order in general chat API route for consistency

--- src/server/routes/api/chats/general/get.ts
+++ src/server/routes/api/chats/general/get.ts
@@ -1,6 +1,6 @@
-import serializeUserAvatar from '#lib/serializeUserAvatar';
 import {FastifyPluginAsyncJsonSchemaToTs} from '@fastify/type-provider-json-schema-to-ts';
 import SQL from 'sql-template-strings';
+import serializeUserAvatar from '#lib/serializeUserAvatar';
 
 const PAGE_SIZE = 50;
 
