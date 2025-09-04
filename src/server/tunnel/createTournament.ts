062e8658f555ea0fdccc2c0d189f38bffd15bfa9
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-09-04 17:38:01 +0200

    Implement tournament name validation in createTournament function

--- src/server/tunnel/createTournament.ts
+++ src/server/tunnel/createTournament.ts
@@ -1,8 +1,24 @@
 import {Client, ClientTunnelMessage} from '#types/Clients';
+import Clients from '#lib/Clients';
 import {FastifyInstance} from 'fastify';
 
 export default async function createTournament(
   server: FastifyInstance,
   client: Client,
   message: ClientTunnelMessage & {type: 'createTournament'},
-) {}
+) {
+  if (typeof message.name !== 'string')
+    return Clients.sendClient(client, {
+      type: 'error',
+      message: 'Invalid tournament name',
+    });
+
+  const name = message.name.trim();
+  if (name.length < 3 || name.length > 64)
+    return Clients.sendClient(client, {
+      type: 'error',
+      message: 'Tournament name must be between 3 and 64 characters',
+    });
+
+  server.tournaments.create(name, client);
+}
