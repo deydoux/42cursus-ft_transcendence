ec074a062800b87b53653892f1acc8649bb33590
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-09-04 15:48:58 +0200

    Remove player state check and error handling from joinTournament function

--- src/server/tunnel/joinTournament.ts
+++ src/server/tunnel/joinTournament.ts
@@ -1,16 +1,8 @@
 import {Client, ClientTunnelMessage} from '#types/Clients';
-import Clients from '#lib/Clients';
 import {FastifyInstance} from 'fastify';
 
 export default async function joinTournament(
   server: FastifyInstance,
   client: Client,
   message: ClientTunnelMessage & {type: 'joinTournament'},
-) {
-  const player = server.game.players[client.userID];
-  if (player)
-    return Clients.sendClient(client, {
-      type: 'error',
-      message: `You are already in a ${player.match?.game === 'tournament' ? 'tournament' : 'match'}`,
-    });
-}
+) {}
