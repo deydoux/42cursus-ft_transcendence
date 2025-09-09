c5bcec2caa7502e1b7b5ad96c306b8617f677fe0
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-09-09 12:37:56 +0200

    fix: Nullify passwordEditedAt when no password set

--- src/server/routes/api/account/get.ts
+++ src/server/routes/api/account/get.ts
@@ -1,13 +1,14 @@
 import {FastifyPluginAsync} from 'fastify';
 import SQL from 'sql-template-strings';
 import serializeUserAvatar from '#lib/serializeUserAvatar';
+import {password} from '#lib/schemas';
 
 const plugin: FastifyPluginAsync = async server => {
   server.get('/', async (request, reply) => {
     const {id} = request.user;
 
     const user = await server.db.get(SQL`
-      SELECT id, username, password_edited_at AS passwordEditedAt,
+      SELECT id, username, password, password_edited_at AS passwordEditedAt,
              totp_enabled AS totp, has_avatar, avatar_version
       FROM users
       WHERE id = ${id}
@@ -17,8 +18,10 @@ const plugin: FastifyPluginAsync = async server => {
 
     user.hasAvatar = Boolean(user.has_avatar);
     user.totp = Boolean(user.totp);
-    if (user.passwordEditedAt)
-      user.passwordEditedAt = new Date(user.passwordEditedAt * 1000);
+    console.log(user.password, !user.password);
+    if (!user.password) user.passwordEditedAt = null;
+    else user.passwordEditedAt = new Date(user.passwordEditedAt * 1000);
+    delete user.password;
     serializeUserAvatar(user);
 
     return reply.send(user);
