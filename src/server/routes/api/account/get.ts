2c9ca649f8002872ab55d8e5bed844b688df6f8e
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-09-25 14:16:46 +0200

    fix: enhance user retrieval query to include elo value and ensure game context

--- src/server/routes/api/account/get.ts
+++ src/server/routes/api/account/get.ts
@@ -7,10 +7,14 @@ const plugin: FastifyPluginAsync = async server => {
     const {id} = request.user;
 
     const user = await server.db.get(SQL`
-      SELECT id, username, password, password_edited_at AS passwordEditedAt,
-             totp_enabled AS totp, has_avatar, avatar_version
-      FROM users
-      WHERE id = ${id}
+      SELECT u.id, username, password, password_edited_at AS passwordEditedAt,
+             totp_enabled AS totp, has_avatar, avatar_version, value AS elo
+      FROM users u
+      JOIN elo e
+      ON u.id = e.user_id
+      WHERE u.id = ${id} AND game = 'pong'
+      ORDER BY e.id DESC
+      LIMIT 1
     `);
 
     if (!user) return reply.notFound('Account not found');
