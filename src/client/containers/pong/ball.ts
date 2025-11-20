104e94061ecaff4577fa60c09281bccd00f076d1
Dorian Eydoux <deydoux@student.42lyon.fr>
2025-11-20 11:23:15 +0100

    fix: adjust maxSpeed calculation for Ball class

--- src/client/containers/pong/ball.ts
+++ src/client/containers/pong/ball.ts
@@ -27,7 +27,7 @@ export class Ball {
     this.radius = ctx.canvas.width * 0.013;
     this.color = 'black';
     this.speed = Math.sqrt(this.vx * this.vx + this.vy * this.vy) * 1.03;
-    this.maxSpeed = Math.max(ctx.canvas.width, ctx.canvas.height) * 0.02;
+    this.maxSpeed = Math.max(ctx.canvas.width, ctx.canvas.height) * 0.012;
     this.isScoring = false;
   }
 
